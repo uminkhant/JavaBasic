2f320ad29fef269916cb15836661c4f2bfa7ccee
MKT <uminkhanthu@gmail.com>
2021-03-22 20:59:13 +0630

    calculator

---
+++ calculator_ui/src/com/jdc/mkt/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.jdc.mkt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class CalculatorController {
+
+    @FXML
+    private Label lb_sign;
+
+    @FXML
+    private Label lb_first;
+
+    @FXML
+    private Label lb_current;
+
+    @FXML
+    private GridPane gd_btn;
+
+}
