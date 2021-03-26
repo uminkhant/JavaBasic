96276b357540c8a68bbca4ab11d8a47850ab8385
MKT <uminkhanthu@gmail.com>
2021-03-26 21:04:31 +0630

    update cal

--- calculator_ui/src/com/jdc/mkt/CalculatorController.java
+++ calculator_ui/src/com/jdc/mkt/CalculatorController.java
@@ -23,22 +23,70 @@ public class CalculatorController implements Initializable {
 	@FXML
 	private GridPane gd_btn;
 
+	private static String ope;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
 		gd_btn.getChildren().stream().filter(e -> e instanceof Button).forEach(btn -> {
 			Button b = (Button) btn;
-			b.setOnAction(e -> showOnLabel(b));
+			b.setOnAction(e -> check(b));
 			return;
 		});
 	}
 
-	private void showOnLabel(Button b) {
-		if (lb_sign.getText().equals("0") || lb_sign.getText() == null || lb_sign.getText().isEmpty()) {
-			lb_current.setText(lb_current.getText().concat(b.getText()));
-			
-		}else {
-			
+	private void check(Button b) {
+
+		if (b.getText().matches("[0-9]")) {
+			showOnLabel(b.getText());
+
+		} else {
+
+			showOnOpeartor(b.getText());
+
+		}
+	}
+
+	private void showOnLabel(String txt) {
+
+		if (!lb_current.getText().equals("0")) {
+			lb_current.setText(lb_current.getText().concat(txt));
+		} else {
+			lb_current.setText(txt);
+		}
+
+	}
+
+	private void showOnOpeartor(String txt) {
+
+		if (!txt.equals("=")) {
+			lb_sign.setText(txt);
+			lb_first.setText(lb_current.getText());
+			lb_current.setText("0");
+		} else {
+			double a=Double.parseDouble(lb_first.getText());
+			double b=Double.parseDouble(lb_current.getText());
+			calculate(a,b,lb_sign.getText());
+		}
+	}
+
+	private void calculate(double a, double b, String text) {
+		switch (text) {
+		case "+":
+			lb_current.setText(String.valueOf(a+b));
+			break;
+		case "-":
+			lb_current.setText(String.valueOf(a-b));
+			break;
+		case "X":
+			lb_current.setText(String.valueOf(a*b));
+			break;
+		case "/":
+			lb_current.setText(String.valueOf(a/b));
+			break;
+
+		default:
+			break;
 		}
 	}
 
